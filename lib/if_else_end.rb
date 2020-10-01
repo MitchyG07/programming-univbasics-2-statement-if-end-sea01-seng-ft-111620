6a39e279584cc62f5da951cd9f3ea6a7070d9d85
bright-storage-1752 <mitchellpgilbert@gmail.com>
2020-10-01 22:40:51 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,3 +1,6 @@
-if TRUE # Write your solution here
-  puts "This code runs!"
+run_code_inside = TRUE
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
 end
+puts "Code after if...end"
