decf7a71974459a537cb8b7de0fb676ed0bd298c
bright-storage-1752 <mitchellpgilbert@gmail.com>
2020-10-01 23:14:52 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,5 @@
 # output if it is currently an even or an odd select_second -- returns epoch time
 current_time = Time.now
-if current_time.to_i / 2 = 0
-  puts "Odd!"
-else
-  puts "Even!"
-end
+exact_second = current_time.to_i
+puts exact_second / 2
   
